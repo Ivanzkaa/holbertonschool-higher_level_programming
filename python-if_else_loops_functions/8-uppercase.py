614db26a4da023640c08c67479e3d3a653bee658
Ivanzkaa <ivanzka@me.com>
2022-09-07 12:14:14 -0400

    fixing some errors

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for if_upper in str:
-        if ord(97) <= ord(if_upper) <= ord(122):
+        if ord(if_upper) >= 97 and ord(if_upper) <= 122:
             if_upper += chr(ord(if_upper) - 32)
             print("{:s}".format(if_upper), end="")
         print("")
