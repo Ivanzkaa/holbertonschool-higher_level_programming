9f9cfeed10bf0b7923b3246f3cb83b4e18ed92f7
Ivanzkaa <ivanzka@me.com>
2022-09-07 12:56:03 -0400

    fixing some errors

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    space = ""
     for if_upper in str:
         if ord(if_upper) >= 97 and ord(if_upper) <= 122:
-            if_upper += chr(ord(if_upper) - 32)
-            print("{}".format(if_upper))
+            space += chr(ord(if_upper) - 32)
+        else:
+            space += if_upper
+    print("{}".format(space))
