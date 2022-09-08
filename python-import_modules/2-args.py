690b846be2e39da312355b51668ee3bab47f38bc
Ivanzkaa <ivanzka@me.com>
2022-09-08 15:11:31 -0400

    listing arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from ast import arg
+
+
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+
+    if length > 2:
+        print("{} arguments:".format(length - 1))
+        for num_args in range(1, length):
+            print("{}: {}".format(num_args, argv[num_args]))
+    elif length == 2:
+        print("{} argument:\n{}: {}".format(1, 1, argv[1]))
+    else:
+            print("0 arguments.")
