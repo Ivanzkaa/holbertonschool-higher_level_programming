84351dfced816f7d72c48d9c2c5b758546b0f06c
Ivanzkaa <ivanzka@me.com>
2022-09-08 22:42:23 -0400

    printing the result of all the arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length_of_arg = len(argv)
+    sum = 0
+    for other_nums in range(1, length_of_arg):
+        num = int(argv[other_nums])
+        sum += num
+        print("{}".format(sum))
