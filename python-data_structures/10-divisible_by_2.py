ad5c22712a2ea18d1226c9a3bbbe785979f8f402
Ivanzkaa <ivanzka@me.com>
2022-09-14 22:43:43 -0400

    a function that the number is divisible by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for mul_nums in my_list:
-        if mul_nums % 2 is 0:
+        if mul_nums % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
