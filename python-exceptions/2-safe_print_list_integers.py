0ee40bbb3d8a7d9cc29bc16b9321e33ac80285ed
Ivanzkaa <ivanzka@me.com>
2022-09-16 22:30:50 -0400

    doing a function that prints x elements of a list and they are only integers while still using try / except

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    numbers = 0
+    for n in range(x):
+        try:
+            print("{:d}".format(my_list[numbers]), end="")
+            numbers += 1
+        except IndexError:
+            numbers +=1
+            continue
+    print()
+    return numbers
