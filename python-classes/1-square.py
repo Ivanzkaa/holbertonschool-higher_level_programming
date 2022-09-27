02a3c7341a49b5c36f4c9b870d04f2f5745ebcdb
Ivanzkaa <ivanzka@me.com>
2022-09-27 18:24:02 -0400

    initializing a private instance for the square class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,11 +3,12 @@
 the class for the square
 """
 
+
 class Square:
     """
     a class for defining a square with a private instance
     """
 
 
-    def __init__(self, square):
-        self.__square = square
+    def __init__(self, size):
+        self.__size = size
