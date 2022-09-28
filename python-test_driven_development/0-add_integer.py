f4a26199b9a9620f2412c2cef739cab763376b18
Ivanzkaa <ivanzka@me.com>
2022-09-28 16:27:18 -0400

    adding integers but if theyre int or float

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+the addition of integers
+"""
+
+
 def add_integer(a, b=98):
+    """
+    addition of 2 integers
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
