d0ba0352e6a6657bb06bc9d3ca18ea2a2d4dfac2
Ivanzkaa <ivanzka@me.com>
2022-09-29 13:53:55 -0400

    initializing and settign the rectangle class

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Before the reactangle class
+"""
+
+
+class Rectangle:
+    """
+    the rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+        initializing tha rectangle class
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width mus be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
