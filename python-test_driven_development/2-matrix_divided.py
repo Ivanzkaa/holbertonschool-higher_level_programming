097d0ad458de8c25d7a9509d0f528c90f7ba4ccf
Ivanzkaa <ivanzka@me.com>
2022-09-28 22:51:08 -0400

    fixing some docmentation errors

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -13,7 +13,7 @@ def matrix_divided(matrix, div):
     for column in range(len(matrix)):
         for row in range(len(matrix[column])):
             if not isinstance(matrix[column][row], (int, float)):
-                raise TypeError("matris ust be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     for row in matrix:
         if len(row) != len(matrix[0]):
@@ -22,7 +22,7 @@ def matrix_divided(matrix, div):
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     elif div == 0:
-        raise ZeroDivisionError("divisio by zero")
+        raise ZeroDivisionError("division by zero")
 
     for i in matrix:
         new_list.append(list(map(lambda i: round(i / div, 2), i)))
