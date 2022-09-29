fa70a4b6f5bfde040ba7b09a030d8aaaafee7c89
Ivanzkaa <ivanzka@me.com>
2022-09-29 13:58:41 -0400

    fixing some typo errors

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("width mus be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
