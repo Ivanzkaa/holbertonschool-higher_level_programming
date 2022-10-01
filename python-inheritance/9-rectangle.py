d72ded1cdd29c0cd7173205f6dd197da6e32a83e
Ivanzkaa <ivanzka@me.com>
2022-10-01 18:00:03 -0400

    adding the area of the rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -23,6 +23,7 @@ class BaseGeometry:
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """
     Rectangle class
