928e3d95b1e8ec9700b8211ecac1a9866ff0af49
Ivanzkaa <ivanzka@me.com>
2022-10-01 17:16:48 -0400

    inheriting from another class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,6 +7,9 @@ inheriting from the base geometry tasks
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
+    """
+    Rectangle class
+    """
     def __init__(self, width, height):
         """
         integer validation
