3184ad961742d1f420c0131c00e1905d14d79d25
Ivanzkaa <ivanzka@me.com>
2022-10-02 13:40:33 -0400

    a function that returns the number of characters written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """
+Before writing from the file
 """
 
 
 def write_file(filename="", text=""):
+    """
+    writing a string to a text file\
+    and returning the number of chars
+    """
     with open(filename, "w") as f:
         new_txt = f.write(text)
     return new_txt
