a975300031546c4f8ba7c343594ab901d45ad427
Ivanzkaa <ivanzka@me.com>
2022-10-02 14:21:38 -0400

    the json string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function of the json string
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    a function that returns the json\
+    representation of an object
+    """
+    return json.dumps(my_obj)
