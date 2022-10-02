c1135500086e137461587b67fc40cf5591ef3fbb
Ivanzkaa <ivanzka@me.com>
2022-10-01 22:26:06 -0400

    a function that adds an attribute

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -9,5 +9,5 @@ def add_attribute(obj, key, value):
     adding an attribute
     """
     if not hasattr(obj, "__slots__") and not hasattr(obj, "__dict__"):
-        raise TypeError("cant' add new attribute")
+        raise TypeError("can't add new attribute")
     setattr(obj, key, value)
