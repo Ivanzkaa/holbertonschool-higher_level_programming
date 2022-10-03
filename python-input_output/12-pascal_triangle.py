fd78e04abd74e562c20061ca7fbca810d45aefba
Ivanzkaa <ivanzka@me.com>
2022-10-03 00:58:18 -0400

    the pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -14,7 +14,7 @@ def pascal_triangle(n):
 
     row = [1]
     x = [0]
-    for i in range(max, (n, 0)):
+    for i in range(max(n, 0)):
         print(row)
         row = [var + other_var for var, other_var in zip(row + x, x + row)]
     return the_list
