e4e743b36053ba1f9631999ed1036ab4d4d5518b
Ivanzkaa <ivanzka@me.com>
2022-10-05 21:55:23 -0400

    adding the raise type errors on the width, height, y and x

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,10 +18,10 @@ class Rectangle(Base):
         call the fucntion and assign the right\
         attributes for the width, height, y and x.
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
