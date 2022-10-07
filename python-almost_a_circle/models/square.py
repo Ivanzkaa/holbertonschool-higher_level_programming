6e6e4ead1aaede131f9053c59a83695ca8fa82e2
Ivanzkaa <ivanzka@me.com>
2022-10-07 16:52:13 -0400

    adding the type error to the square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,4 @@ class Square(Rectangle):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be > 0")
+        self.__size = value
