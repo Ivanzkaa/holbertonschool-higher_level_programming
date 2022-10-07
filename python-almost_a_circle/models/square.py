f2c341db4558ea84d0e8ddd316b16fd908c691ff
Ivanzkaa <ivanzka@me.com>
2022-10-07 13:51:33 -0400

    adding the super innit and calling the attributes from the rectangle class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""""""
+"""Before the inheritance from the rectangle class"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """"""
+    """the creation of the square class and calling the supper innit"""
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         super().__init__ (size, size, x=x, y=y, id=id)
 
 
     def __str__(self):
-        """"""
+        """returning the square"""
         return "[Square] ({}) ({})/({}) - ({})".format(
             self.id, self.x, self.y, self.size)
