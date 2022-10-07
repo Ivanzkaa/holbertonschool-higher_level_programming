f3fb04cdfed44b494aed9f5853165d9e8f7f44bc
Ivanzkaa <ivanzka@me.com>
2022-10-07 17:54:24 -0400

    the json dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Before creating the base class
 """
 
 
+import json
+
+
 class Base:
     """
     The creation of the base class
@@ -20,3 +23,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """the json string representation of list_\
+        of dictionaies"""
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
