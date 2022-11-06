18dc721893ba09f04dbb9d14ed45b5b1d357e71a
Ivanzkaa <ivanzka@me.com>
2022-11-05 21:20:06 -0400

    forgot to add double quotes

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS hbtn_test_db_9.second_table (
 
 INSERT INTO second_table (id, name, score)
 VALUES 
-(1, John, 10)
-(2, Alex, 3)
-(3, Bob, 14)
-(4 George, 8);
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4 "George", 8);
