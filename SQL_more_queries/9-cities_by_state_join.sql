070fc41f4dffa777e48157b062dae0a5e1f030b6
Ivanzkaa <ivanzka@me.com>
2022-11-06 23:39:29 -0400

    lisiting the cities

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
--- Listing all cities
-SELECT c.id, c.name, s.name
+-- Listing all cities with their state
+SELECT C.id, C.name, S.name
 FROM cities AS C
 INNER JOIN states AS S
 ON C.state_id = S.id
-ORDER BY c.id;
+ORDER BY C.id ASC;
