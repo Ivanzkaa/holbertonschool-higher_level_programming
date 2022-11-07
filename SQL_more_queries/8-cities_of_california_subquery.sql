a394dfa676368887055f1dbc287485606861a324
Ivanzkaa <ivanzka@me.com>
2022-11-06 22:57:28 -0400

    listing all of the cities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- List of all cities of California that can be found in the database
 SELECT id, name
 FROM cities
-WHERE state_id (
+WHERE state_id = (
     SELECT id
     FROM states
     WHERE name = "California"
