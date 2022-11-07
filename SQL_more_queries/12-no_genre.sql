55f2dda6d6a7f55fb50c2587347970fce7aa4761
Ivanzkaa <ivanzka@me.com>
2022-11-06 23:57:20 -0400

    listing all of the shows contained in a database

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Listing all of the shows in a database without a genre linked
+SELECT t_s.title, t_s_g.genre_id
+FROM tv_show_genres AS t_s_g
+RIGHT JOIN tv_shows AS t_s
+ON t_s_g.show_id = t_s.id
+WHERE t_sG.genre_id is NULL
+ORDER BY t_s.title ASC,
+t_s_g.genre_id;
