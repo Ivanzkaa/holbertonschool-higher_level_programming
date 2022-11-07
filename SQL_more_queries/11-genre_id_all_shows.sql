4eb793c8218ae75571a171fa44898aa57214ef5a
Ivanzkaa <ivanzka@me.com>
2022-11-06 23:53:25 -0400

    listing all of the shows in the database

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Listing all the shows contained in the database
+SELECT t_s.title, t_s_g.genre_id
+FROM tv_show_genres AS t_s_g
+RIGHT JOIN tv_shows AS t_s
+ON t_s_g.show_id = t_s.id
+ORDER BY t_s.title ASC,
+t_s_g.genre_id;
