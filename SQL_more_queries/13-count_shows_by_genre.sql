8767a8d8d924fb37f28a04711a3a885572923f17
Ivanzkaa <ivanzka@me.com>
2022-11-07 00:23:14 -0400

    listing all ofthe genres

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Listing all of the genres from the database and displays the number of shows linked to each
-SELECT t_g.name AS genre,
+SELECT g.name AS genre,
 COUNT(*) AS number_of_shows
 FROM tv_genres AS t_g
-INNER JOIN tv_show_genres AS s_g
-ON t_s_g.genre_id = t_g.id
-GROUP BY t_g.name
+INNER JOIN tv_show_genres AS t_s_g
+ON g.id = t_s_g.genre_id
+GROUP BY g.name
 ORDER BY number_of_shows DESC;
