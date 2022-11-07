3bc725fba1084bab3ab151ff033b5043153e1446
Ivanzkaa <ivanzka@me.com>
2022-11-06 20:15:40 -0400

    creating a table with a unique

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Creating another table with a unique value
-CREATE TABLE IF NOT EXISTS unique_id (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_5.unique_id (
     id INT(1) UNIQUE
     name VARCHAR(256)
 );
