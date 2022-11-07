bc14401b685b19327f1faf7f3e40f2b5222f9d42
Ivanzkaa <ivanzka@me.com>
2022-11-06 22:21:53 -0400

    creating another database and table, adding a foreign key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Creating a database and the table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE NOT NULL AUTO_INCREMENTED,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id)
+);
