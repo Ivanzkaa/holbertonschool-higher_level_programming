2ceaf3df06eb21714e3553e07e4ad86341ae35b8
Ivanzkaa <ivanzka@me.com>
2022-11-07 00:38:27 -0400

    listing the genres of the show dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,7 @@ SELECT t_g.name
 FROM tv_show_genres AS t_s_g
 INNER JOIN tv_shows AS t_s
 ON t_s_g.show_id = t_s.id
-INNER JOIN tv_genre_id = t_g.id
+INNER JOIN tv_genre_id AS t_g
+ON t_s_g.genre_id = t_g.id
 WHERE t_s.title = "Dexter"
 ORDER BY t_g.name ASC;
