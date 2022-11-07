6ee39db08e944f578a5e2061c3eea55632d63979
Ivanzkaa <ivanzka@me.com>
2022-11-06 23:58:04 -0400

    listing all of the shows contained in a database

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@ SELECT t_s.title, t_s_g.genre_id
 FROM tv_show_genres AS t_s_g
 RIGHT JOIN tv_shows AS t_s
 ON t_s_g.show_id = t_s.id
-WHERE t_sG.genre_id is NULL
+WHERE t_s_g.genre_id is NULL
 ORDER BY t_s.title ASC,
 t_s_g.genre_id;
