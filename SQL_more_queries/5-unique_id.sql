5fe0e565e4eb5afc3d9026f07e557e5fa59bbf47
Ivanzkaa <ivanzka@me.com>
2022-11-06 20:19:34 -0400

    adding default value

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Creating another table with a unique value
 CREATE TABLE IF NOT EXISTS hbtn_test_db_5.unique_id (
-    id INT(1) NOT NULL UNIQUE,
+    id INT NOT NULL UNIQUE DEFAULT 1,
     name VARCHAR(256)
 );
