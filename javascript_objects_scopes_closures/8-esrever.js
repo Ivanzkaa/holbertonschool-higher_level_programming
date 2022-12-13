38e7e1ab601302a4b677c7370f9f65a954cd3fa1
Ivanzkaa <ivanzka@me.com>
2022-12-13 14:17:11 -0400

    returning a reversed list

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list){
-    let new_list = [];
+    const new_list = [];
     for (let i = list.length - 1; i >= 0; i--) {
         new_list.push(list[i]);
     }
