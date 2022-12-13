5df12da8bb5778a6a5c222dc4ab386081e0aceb7
Ivanzkaa <ivanzka@me.com>
2022-12-12 22:47:16 -0400

    fixing an error

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-let multi_lang = ["C is fun",
-"Python is cool",
-"JavaScritp is amazing"];
+let multi_lang = ["C is fun", "Python is cool", "JavaScritp is amazing"];
 
 for (let i = 0; i < multi_lang.length; i++) {
     console.log(multi_lang[i]);
