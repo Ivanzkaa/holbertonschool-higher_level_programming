34f83fe39ee322601e06703151e631d883a05916
Ivanzkaa <ivanzka@me.com>
2022-12-15 19:50:41 -0400

    a script that reads and print the content of the file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const fileDest = process.argv[2];
-
-fs.readFile(fileDest, "utf8", function (error, data) {
-    if (error) {
-        console.log(error);
-    } else {
-        process.stdout.write(data);
-    }
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+    console.log(error || content);
 });
