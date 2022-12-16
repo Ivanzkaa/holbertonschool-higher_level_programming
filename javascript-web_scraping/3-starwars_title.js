e401f0bcb847dc40152501c528784c2350224e8c
Ivanzkaa <ivanzka@me.com>
2022-12-15 21:54:04 -0400

    a script that prints a title ofa star wars movie

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-
-const request = require("request");
-let url = "https://swapi-api.hbtn.io/api/films/:id" + process.argv[2];
+const request = require('request');
+let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
     console.log(error || JSON.parse(body).title);
 });
