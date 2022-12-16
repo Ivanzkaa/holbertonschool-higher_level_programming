9a739e94cfd25030cc5b2c1f90a4d34d7539b7ab
Ivanzkaa <ivanzka@me.com>
2022-12-15 20:48:51 -0400

    a script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-    if (error) console.log(error);
+const fileDest = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileDest, content, "utf-8", function (err, data) {
+    if (err) {
+        console.log(err);
+    }
 });
