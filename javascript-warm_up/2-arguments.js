0b9ee0117850a327c48ad2913b9765c0376e8544
Ivanzkaa <ivanzka@me.com>
2023-02-17 12:32:01 -0400

    the number of arguments passed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const process = require('process');
 
 if (process.argv[3]) {
-    console.log("Arguments found")
+    console.log("Arguments found");
 } else if (process.argv[2]) {
-    console.log("Argument found")
+    console.log("Argument found");
 } else {
     console.log("No argument");
 }
